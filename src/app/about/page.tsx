021babd57978e980bfa64e27e3985321a91f2914
RedietNegash <redietnegash9@gmail.com>
2024-12-30 10:04:54 +0300

    change background image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,7 +15,7 @@ const About = () => {
       <div className="relative min-h-[90vh] font-secondary h-fit ">
         <div className="w-full object-cover object-center h-[90vh] absolute top-[-50px] left-0  bg-tertiary/70 z-20"></div>
         <div
-          style={{ backgroundImage: `url(/aboutHero.jpg)` }}
+          style={{ backgroundImage: `url(/about.jpg)` }}
           className=" absolute top-[-50px] left-0  bg-cover w-full h-[90vh] object-contain object-center  z-0"
         ></div>
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-90%] sm:translate-y-[-50%] font-secondary z-20 flex  flex-col w-[90%] sm:w-[70%] ">
